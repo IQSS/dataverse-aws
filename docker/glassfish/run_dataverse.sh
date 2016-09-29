3ed7f6431a5ff19c5e314010ad70a60aa3546273
Joel Anderson <janderson@vtti.vt.edu>
2016-09-29 14:24:50 -0400

    shift glassfish ports until DOI un/pw in place

--- docker/glassfish/run_dataverse.sh
+++ docker/glassfish/run_dataverse.sh
@@ -3,6 +3,9 @@
 ASADMIN=${GLASSFISH_ROOT}/bin/asadmin
 
 dvinstall/glassfish-setup.sh
+# use non-standard ports to make service unavailable until correct DOI credentials in place
+${ASADMIN} set server.http-service.http-listener.http-listener-1.port=60001
+${ASADMIN} set server.http-service.http-listener.http-listener-2.port=60002
 ${ASADMIN} deploy dvinstall/dataverse.war
 
 ulimit -n 32768
@@ -18,4 +21,6 @@ LANG=en_US.UTF-8; export LANG
 "${ASADMIN}" create-jvm-options "-Ddoi.password=${DOI_PASSWORD}"
 
 "${ASADMIN}" stop-domain "${GLASSFISH_DOMAIN}"
+${ASADMIN} set server.http-service.http-listener.http-listener-1.port=8080
+${ASADMIN} set server.http-service.http-listener.http-listener-2.port=8181
 "${ASADMIN}" start-domain --verbose "${GLASSFISH_DOMAIN}"
